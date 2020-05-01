64d54d62748c5de8c5d6aacd4f33220fd739fda7
teamrab <fbglcom@gmail.com>
2020-04-30 22:29:55 -0700

    make it better

--- homes.php
+++ homes.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?>
+   class Emp {
+      public $name = "";
+      public $hobbies  = "";
+      public $birthdate = "";
+   }
+	
+   $e = new Emp();
+   $e->name = "sachin";
+   $e->hobbies  = "sports";
+   $e->birthdate = date('m/d/Y h:i:s a', "8/5/1974 12:20:03 p");
+   $e->birthdate = date('m/d/Y h:i:s a', strtotime("8/5/1974 12:20:03"));
 
-</body>
-</html>
+   echo json_encode($e);
+?>
